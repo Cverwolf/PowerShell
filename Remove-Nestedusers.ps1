dfa7d8ce4c091e8e23c4f095a7a4147c0739bb2d
joefunk <joe.c.funk@gmail.com>
2015-08-13 16:13:00 -0400

    Update Remove-Nestedusers.ps1

--- Remove-Nestedusers.ps1
+++ Remove-Nestedusers.ps1
@@ -1,20 +1,40 @@
-# THIS FUNCTION WILL COLLAPSE NESTED GROUP MEMBERSHIP ONE LEVEL UP.
+<#
+.SYNOPSIS
 
-# Joe Funk - 8/13/2015
+This function will collapse nested group membership one level up to facillitate decommission a large number of groups
 
-# The script takes an input of groups, then it checks those groups for membership in any other group (Memberof) 
-# If it finds a group is a member of any other groups it moves forward with processing
-# It recusrsively checks its own group membership for users, then adds all those users to any and all groups that it is a direct member of.  
-# Lastly it removes itself from the groups that it is a memberof
+.DESCRIPTION
 
-# Effectively it is replacing any membership it has in any groups, with direct membership of its own members including users in groups nested in itself.
-# Thus, collapsing nesting in lieu of direct membership
+The script takes an input of groups, then it checks those groups for membership in any other group (Memberof) 
+If it finds a group is a member of any other groups it moves forward with processing
+It recusrsively checks its own group membership for users, then adds all those users to any and all groups that it is a direct member of.  
+Lastly it removes itself from the groups that it is a memberof
 
-# This is most useful for decommissioning a large number of groups
 
-# It can take pipeline input for the 'strGroups' variable.  
-# For example :  
-# $strgroups = Get-ADGroup -Filter {GroupCategory -eq 'security' -and name -like '*test*'} -SearchBase 'OU=Test,OU=Groups,DC=PHYSIOCORP,DC=COM' 
+.PARAMETER StrGroups
+
+Group name to interrogate and then collapse nested membership one level up from
+
+.EXAMPLE
+
+Collapse nested membership from a single group called 'test_group'
+
+remove-nestedusers test_group
+
+.EXAMPLE
+
+Collapse nested membership via pipeline input from variable
+
+$strgroups = Get-ADGroup -Filter {GroupCategory -eq 'security' -and name -like '*test*'} -SearchBase 'OU=Test,OU=Groups,DC=domain,DC=COM'
+remove-nestedusers $StrGroups
+
+.NOTES
+
+It can take pipeline input for the 'strGroups' variable.  
+Joe Funk - 8/13/2015
+#>
+
+
 
 function remove-nestedusers {
 
@@ -35,14 +55,14 @@ foreach ($strGroup in $strgroups)
 {
 
 # Determine if group is a member of any other groups, if it is then it moves forwards with collapsing nesting
-if ((GET-ADGROUP –Identity $strGroup –Properties MemberOf | Select-Object MemberOf).memberof)
+  if ((GET-ADGROUP –Identity $strGroup –Properties MemberOf | Select-Object MemberOf).memberof)
 
 {
 
 # List groups that current group is a member of and save to variable
-$CurrentGroupGroups = (GET-ADGROUP –Identity $strGroup –Properties MemberOf | Select-Object MemberOf).MemberOf
+  $CurrentGroupGroups = (GET-ADGROUP –Identity $strGroup –Properties MemberOf | Select-Object MemberOf).MemberOf
 # List membership of current group and save to variable
-$members = Get-ADGroupMember $strgroup -recursive
+  $members = Get-ADGroupMember $strgroup -recursive
 
 
 foreach($group in $CurrentGroupGroups)
