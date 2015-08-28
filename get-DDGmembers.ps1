777466eb587e6d61efccf48a5690de7bfd9b9a92
joefunk <joe.c.funk@gmail.com>
2015-08-28 09:45:37 -0400

    Create get-DDGmembers.ps1

---
+++ get-DDGmembers.ps1
@@ -0,0 +1,42 @@
+function get_DDMems($DDGgrps)
+{
+foreach ($grp in $DDGgrps) 
+{
+    $DDGmembers = get-recipient -recipientpreviewfilter $grp.RecipientFilter -resultsize unlimited | select name, primarysmtpaddress 
+    $grp.Name + " has the following " + @($DDGmembers).count + " members: `n"
+    "----------------------------------------------------------"
+    @($DDGmembers) + "`r`n"
+}
+} 
+
+#Check for and if needed, load Exchange 2007 snapin
+if ( (Get-PSSnapin -Name Microsoft.Exchange.Management.PowerShell.Admin -ErrorAction SilentlyContinue) -eq $null )
+{
+    Add-PSSnapin Microsoft.Exchange.Management.PowerShell.Admin
+}
+
+
+#Create choice menu
+$title = "DDG Membership"
+$message = "Export DDG Members - Specific DDG or all DDGs"
+$all = New-Object System.Management.Automation.Host.ChoiceDescription "&All DDGs", "Export members of all DDGs"
+$specificDDG = New-Object System.Management.Automation.Host.ChoiceDescription "&Specific DDG", "Export members of a specific DDG"
+$options = [System.Management.Automation.Host.ChoiceDescription[]]($All, $specificDDG)
+$result = $host.ui.PromptForChoice($title, $message, $options, 0) 
+
+switch ($result)
+    {
+        0 {
+            $DDGgrps = Get-DynamicDistributionGroup -resultsize unlimited
+            get_DDMems $DDGgrps | out-file All_DDG_members.txt
+          }
+        1 {
+            [System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') | Out-Null
+            $inputgrp = [Microsoft.VisualBasic.Interaction]::InputBox("Enter a Dynamic Disto Group name", "DDG") 
+            $DDGgrps = Get-DynamicDistributionGroup $inputgrp
+            $outfile = $inputgrp + "_DDG_members.txt"
+            get_DDMems $DDGgrps | out-file $outfile
+          }
+    }
+
+ 
